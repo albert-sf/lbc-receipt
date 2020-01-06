ee966e566f61685d9161f9ebf0f3e764729b0ba5
systembugtj <albert_lee@hotmail.com>
2020-01-06 12:36:53 -0800

    enable snapshot test

--- src/c/button/__unit__/button.spec.js
+++ src/c/button/__unit__/button.spec.js
@@ -8,7 +8,16 @@ const createButton = (props = {}) => {
     return element;
 };
 
-describe('c-button attributes', () => {
+describe('c-button', () => {
+    it('should render', () => {
+        const title = 'Click here';
+        const element = createButton({ title });
+
+        return Promise.resolve().then(() => {
+            expect(element).toMatchSnapshot();
+        });
+    });
+
     it('should set title attribute on the button', () => {
         const title = 'Click here';
         const element = createButton({ title });
